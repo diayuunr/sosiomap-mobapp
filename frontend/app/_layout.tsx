68a7008f066d3240b163a71945e8b819cb853cbb
diayuunr <diayunuraini9@gmail.com>
2026-05-16 19:07:45 +0700

    style: adjust navbar to absolute

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Tabs } from "expo-router";
 import Feather from "@expo/vector-icons/Feather";
 import { useFonts } from "expo-font";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
   const [fontsLoaded] = useFonts({
     MplusBlack: require("../assets/font/MPLUSRounded1c-Black.ttf"),
     MplusExtraBold: require("../assets/font/MPLUSRounded1c-ExtraBold.ttf"),
@@ -27,31 +30,37 @@ export default function TabLayout() {
         tabBarLabelStyle: {
           fontFamily: "MplusBold",
           fontSize: 12,
-          marginTop: 0,
+          marginTop: 2,
         },
 
         tabBarStyle: {
           position: "absolute",
 
-          bottom: 40,
-          marginHorizontal: 20,
-
-          paddingTop: 7,
-          paddingBottom: 7,
-
-          elevation: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
 
           backgroundColor: "white",
 
-          borderRadius: 100,
+          borderTopWidth: 1,
+          borderTopColor: "#f1f5f9",
 
-          height: 70,
-
-          borderTopWidth: 0,
+          elevation: 8,
 
           shadowColor: "#000",
-          shadowOpacity: 0.1,
-          shadowRadius: 10,
+          shadowOpacity: 0.05,
+          shadowRadius: 8,
+
+          paddingTop: 6,
+
+          // IMPORTANT
+          paddingBottom: insets.bottom > 0 ? insets.bottom : 10,
+
+          height: 60 + (insets.bottom > 0 ? insets.bottom : 10),
+        },
+
+        tabBarItemStyle: {
+          paddingVertical: 4,
         },
       }}
     >
@@ -60,7 +69,7 @@ export default function TabLayout() {
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => (
-            <Feather name="home" size={24} color={color} />
+            <Feather name="home" size={22} color={color} />
           ),
         }}
       />
@@ -70,7 +79,7 @@ export default function TabLayout() {
         options={{
           title: "Maps",
           tabBarIcon: ({ color }) => (
-            <Feather name="map" size={24} color={color} />
+            <Feather name="map" size={22} color={color} />
           ),
         }}
       />
@@ -80,7 +89,7 @@ export default function TabLayout() {
         options={{
           title: "Export",
           tabBarIcon: ({ color }) => (
-            <Feather name="file" size={24} color={color} />
+            <Feather name="file" size={22} color={color} />
           ),
         }}
       />
